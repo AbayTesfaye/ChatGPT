5d282217383d11157787bfcf2b56ca5dc07a241f
AbayTesfaye <abaytesfaye02@gmail.com>
2024-10-24 04:23:16 -0700

    HTML structure for Sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src="" alt="" className="logo" />
+            <span className="brand">ChatGPT</span>
+          </div>
+          <button className="midBtn">
+            <img src="" alt="" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src="" alt="" />
+              What is Programming?
+            </button>
+            <button className="query">
+              <img src="" alt="" />
+              What is Programming?
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
 }
 
 export default App;
